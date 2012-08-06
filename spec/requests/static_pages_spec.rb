1a830fa4c03be4737a67cb7852a0f57a66511c33
Brendan Yong <brendanyong@gmail.com>
2012-08-07 01:30:13 +0800

    Added Contact Page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,7 +4,7 @@ describe "Static pages" do
 
 	describe "Home page" do
 
-		it "should have the content 'Sample App'" do
+		it "should have the h1 'Sample App'" do
   		
 			visit '/static_pages/home'
 			page.should have_selector('h1', :text => 'Sample App')
@@ -20,7 +20,7 @@ describe "Static pages" do
 
   describe "Help page" do
 
-		it "should have the content 'Help'" do
+		it "should have the h1 'Help'" do
   		
 			visit '/static_pages/help'
 			page.should have_selector('h1', :text => 'Help')
@@ -36,7 +36,7 @@ describe "Static pages" do
 
   describe "About page" do
 
-		it "should have the content 'About Us" do
+		it "should have the h1 'About Us" do
   		
 			visit '/static_pages/about'
 			page.should have_selector('h1', :text => 'About Us')
@@ -50,5 +50,22 @@ describe "Static pages" do
     end
   end
 
-  
+describe "Contact page" do
+
+    it "should have the h1 'Contact" do
+      
+      visit '/static_pages/contact'
+      page.should have_selector('h1', :text => 'Contact')
+    end
+
+    it "should have the right title" do
+
+      visit '/static_pages/contact'
+      page.should have_selector('title', 
+                      :text => "Ruby on Rails Tutorial Sample App | Contact")
+    end
+end
+
+
+
 end
