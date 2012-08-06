8ca163cfbc26d39993d82014bcb19641ccd9dab6
Brendan Yong <brendanyong@gmail.com>
2012-08-07 00:21:16 +0800

    changed content to selector

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,25 +7,25 @@ describe "Static pages" do
 		it "should have the content 'Sample App'" do
   		
 			visit '/static_pages/home'
-			page.should have_content('Sample App')
-  		end
+			page.should have_selector('h1', :text => 'Sample App')
   	end
+  end
 
-  	describe "Help page" do
+  describe "Help page" do
 
 		it "should have the content 'Help'" do
   		
 			visit '/static_pages/help'
-			page.should have_content('Help')
-  		end
+			page.should have_selector('h1', :text => 'Help')
   	end
+  end
 
-  	describe "About page" do
+  describe "About page" do
 
 		it "should have the content 'About Us" do
   		
 			visit '/static_pages/about'
-			page.should have_content('About Us')
-  		end
+			page.should have_selector('h1', :text => 'About Us')
   	end
+  end
 end
